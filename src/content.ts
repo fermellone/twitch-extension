f38a78018bab017975d3c24af060543495af82e6
Fer Mellone <fermellone@gmail.com>
2024-09-14 21:40:12 -0400

    implement shadcn and running cycle

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,22 @@
 console.log('Content script loaded');
 
+let isUsersChannel = false;
+
 chrome.storage.local.get(['channelName'], (result) => {
-	console.log('asd', result);
+	console.log('asd', result.channelName);
+
+	isUsersChannel = window.location.href.includes(result.channelName);
 });
+
+const interval = setInterval(() => {
+	if (!isUsersChannel) {
+		return;
+	}
+
+	console.log('isUsersChannel', isUsersChannel);
+}, 1000);
+
+window.onbeforeunload = () => {
+	console.log('cleaning interval');
+	clearInterval(interval);
+};

336002e2d97842e5525544cffc0f6df420387968
Fer Mellone <fermellone@gmail.com>
2024-09-14 21:05:30 -0400

    add content script

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'sveltekit-adapter-chrome-extension';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { fileURLToPath } from 'node:url';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,11 +8,30 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	build: {
+		rollupOptions: {
+			input: {
+				content: fileURLToPath(new URL('./src/content.ts', import.meta.url))
+			}
+		}
+	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			manifest: 'manifest.json',
+			static: {
+				include: ['static']
+			}
+		}),
+		appDir: 'app'
 	}
 };
 
